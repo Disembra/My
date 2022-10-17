336a4ea14ede78ee320cda891b78eb938fbc13ac
Disembra <92911748+Disembra@users.noreply.github.com>
2022-10-17 18:18:41 +0500

    dcvfgbhnjm

---
+++ source/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Фило'
+copyright = '2022, Anyashka'
+author = 'Anyashka'
+release = '1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+language = 'ru'
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,21 @@
+.. MySphinx documentation master file, created by
+   sphinx-quickstart on Mon Oct 10 13:17:26 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Добро пожаловать в "Фило:искусство музыки"
+==========================================
+
+
+.. toctree::
+   usage
+   bilet
+   
+
+   :maxdepth: 3
+
+
+
+
+
+
